216e541c5aa8014c4f1473d68e8ba04c139c24f9
Pack Heng <pack@oz-heng.com>
2017-08-19 16:25:50 +1000

    Uses ButterKnife tool Added an EditTextFragment: Pressing the "Submit" button within this Fragment increments the score value which TextView belongs to the activity_main Layout.  On branch 001_Add_a_Fragment  Changes to be committed: 	deleted:    .idea/.name 	modified:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/runConfigurations.xml 	new file:   .idea/vcs.xml 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/oz_heng/apps/android/ozquiz/EditTextFragment.java 	modified:   app/src/main/java/com/oz_heng/apps/android/ozquiz/MainActivity.java 	modified:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/layout/fragment_edit_text.xml 	new file:   app/src/main/res/values/dimens.xml

--- app/src/main/java/com/oz_heng/apps/android/ozquiz/MainActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/ozquiz/MainActivity.java
@@ -1,13 +1,59 @@
 package com.oz_heng.apps.android.ozquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity
+        implements EditTextFragment.OnFragmentInteractionListener {
+
+    // User score
+    private int mScore = 0;
+
+    @BindView(R.id.score) TextView mScoteTextView;
+//    @BindView(R.id.fragment_edit_text) FrameLayout frameLayoutEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Display user score.
+        displayScore(mScore);
+
+        // If the activity isn't using the 'fragment_edit_text' layout, them create an
+        // EditTextFragment.
+        if (findViewById(R.id.fragment_edit_text) == null) {
+
+            EditTextFragment editTextFragment = new EditTextFragment();
+
+            // In case this fragment was started with special instructions from an Intent,
+            // pass the Intent's extras to the fragment as arguments
+            editTextFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'activity_container' layout
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                    editTextFragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Boolean isCorrect) {
+        if (isCorrect) {
+            mScore ++;
+            displayScore(mScore);
+        }
+    }
+
+    /**
+     * Display user score.
+     * @param score score to display.
+     */
+    private void displayScore(int score) {
+        mScoteTextView.setText(String.valueOf(mScore));
     }
 }
